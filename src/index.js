76a1a695821278870e70a6c124260678d04a673e
bin.zeng <yy282371715@sina.cn>
2024-03-24 00:02:49 +0800

    update: 更新

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { name, version, repository } from '../package.json'
+import { name, version, homepage } from '../package.json'
 
 console.log(
-	`%c ${name} %c v${version} %c ${repository.url}`,
+	`%c ${name} %c v${version} %c ${homepage}`,
 	'padding: 2px 1px; border-radius: 3px 0 0 3px; color: #fff; background: #606060; font-weight: bold;',
 	'padding: 2px 1px; border-radius: 0 0 0 0; color: #fff; background: #42c02e; font-weight: bold;',
 	'padding: 2px 2px 2px 2px; border-radius: 0 3px 3px 0; color: #fff; background: #ffc3dc; font-weight: bold;'
